819020752d0bae9fbe08169c84cc7b7bfbd09654
Marcel Luethi <marcel.luethi@inf.ethz.ch>
2026-01-08 14:35:35 +0100

    add fixture that downloads the data

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from src import download_penguins_data
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_data():
+    """
+        Download penguins data before running tests.
+        This 'fixture' is run automatically before the test session starts
+    """
+    download_penguins_data()
+    yield
